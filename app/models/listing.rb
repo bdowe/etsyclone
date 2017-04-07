47c4fbf14d577895ad6d2e11f9c819afe798f0ad
Brian Dowe <briandowe@gmail.com>
2017-04-06 17:57:21 -0700

    Add validations for Users and Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ApplicationRecord
 	mount_uploader :image, ImageUploader
+	validates :name, :description, :price, :image, presence: true
+ 	validates :price, numericality: { greater_than: 0 }
 end
