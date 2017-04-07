7c715309b820ced460d79768263e2ef64f98176a
Brian Dowe <briandowe@gmail.com>
2017-04-07 08:17:36 -0700

    Associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,6 @@ class Listing < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	validates :name, :description, :price, :image, presence: true
  	validates :price, numericality: { greater_than: 0 }
+
+ 	belongs_to :user
 end
